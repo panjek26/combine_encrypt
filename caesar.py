a7cc04dff0af73b2e13fc3d71cec39dcbf82a8f0
panji.kurniawan <panjipratamakurniawan001@gmail.com>
2023-03-20 08:10:04 +0700

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
 import os
 
 def generate_key():
-    key = Fernet.generate_key()
+    key = os.urandom(32)
     with open("secret.key", "wb") as key_file:
         key_file.write(key)
 
@@ -16,16 +15,12 @@ def encrypt_data(data, key):
     # Generate a random initialization vector (IV)
     iv = os.urandom(16)
 
-    # Create a cipher object with AES algorithm and CBC mode
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
-
-    # Pad the data
-    padder = padding.PKCS7(128).padder()
-    padded_data = padder.update(data.encode()) + padder.finalize()
+    # Create a cipher object with Blowfish algorithm and CBC mode
+    cipher = Cipher(algorithms.Blowfish(key), modes.CBC(iv), backend=default_backend())
 
     # Encrypt the data using the cipher object
     encryptor = cipher.encryptor()
-    encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+    encrypted_data = encryptor.update(data.encode()) + encryptor.finalize()
 
     # Combine the IV and encrypted data
     combined_data = iv + encrypted_data
@@ -44,7 +39,7 @@ if __name__ == '__main__':
     main()
 
 # Streamlit app
-st.title("AES Encryption")
+st.title("Blowfish Encryption")
 
 st.header("Encrypt some data")
 
