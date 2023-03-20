8c85372fc31882b9a53f20f70a35b638fab54cc4
panji.kurniawan <panjipratamakurniawan001@gmail.com>
2023-03-20 08:35:59 +0700

    update

---
+++ LAT_Combine.py
@@ -0,0 +1,68 @@
+import streamlit as st
+
+def caesar_cipher(text, shift):
+    result = ''
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                result += chr((ord(char) + shift - 65) % 26 + 65)
+            else:
+                result += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            result += char
+    return result
+
+def substitution_cipher(text, key):
+    result = ''
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                result += key[ord(char) - 65].upper()
+            else:
+                result += key[ord(char) - 97]
+        else:
+            result += char
+    return result
+
+def combine_cipher(text, shift, key):
+    text = caesar_cipher(text, shift)
+    text = substitution_cipher(text, key)
+    return text
+
+def main():
+    st.title("Cipher")
+    option = st.sidebar.selectbox("Select the Cipher", ["Caesar Cipher", "Substitution Cipher", "Combine Cipher"])
+
+    if option == "Caesar Cipher":
+        st.subheader("Encrypt using Caesar Cipher")
+        text = st.text_input("Enter the text to be encrypted", "")
+        shift = st.number_input("Enter the shift value", value=0, step=1, min_value=0, max_value=25)
+        if st.button("Encrypt"):
+            encrypted_text = caesar_cipher(text, shift)
+            st.success("Encrypted Text: {}".format(encrypted_text))
+
+    elif option == "Substitution Cipher":
+        st.subheader("Encrypt using Substitution Cipher")
+        text = st.text_input("Enter the text to be encrypted", "")
+        key = st.text_input("Enter the key (26 alphabets only)", "")
+        if st.button("Encrypt"):
+            if len(key) != 26:
+                st.error("Key should have 26 alphabets only")
+            else:
+                encrypted_text = substitution_cipher(text, key)
+                st.success("Encrypted Text: {}".format(encrypted_text))
+
+    elif option == "Combine Cipher":
+        st.subheader("Encrypt using Combine Cipher")
+        text = st.text_input("Enter the text to be encrypted", "")
+        shift = st.number_input("Enter the shift value", value=0, step=1, min_value=0, max_value=25)
+        key = st.text_input("Enter the key (26 alphabets only)", "")
+        if st.button("Encrypt"):
+            if len(key) != 26:
+                st.error("Key should have 26 alphabets only")
+            else:
+                encrypted_text = combine_cipher(text, shift, key)
+                st.success("Encrypted Text: {}".format(encrypted_text))
+
+if __name__ == '__main__':
+    main()
